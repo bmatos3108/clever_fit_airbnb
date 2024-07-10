18ccb8e1414577b42c1d6016342245abd90a6a4e
matt shilling <matthewds@sky.com>
2024-07-10 11:08:07 +0100

    Fixed the routes for index and made it cleaner overall

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,33 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  #resources :users, only: [:show, :index] do
-   # resources :reviews, only: [:index]  # Nested route to show reviews for a user
-    #resources :services, only: [:index] # Nested route to show services created by a user
-  #end
 
-  # Resources for Bookings
-  resources :bookings
-
-  # Resources for Services
-  resources :services do
-    resources :reviews, only: [:index, :create, :new] # Nested route to show and create reviews for a service
+  resources :users, only: %i[show index] do
+    member do
+      get 'chef', to: 'users#chef'
+    end
+    resources :reviews, only: [:index]
+    resources :services, only: [:index]
   end
 
-  # Resources for Reviews
-  resources :reviews, only: [:show, :edit, :update, :destroy]
 
-  # Root path
- # root to: "home#index"
+  resources :bookings do
+    collection do
+      get 'past', to: 'bookings#past'
+    end
+  end
+
+  resources :services, except: [:index] do
+    resources :reviews, only: %i[create new] # Nested route to show and create reviews for a service
+  end
+
+  resources :reviews, only: %i[show edit update destroy]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  #get "up" => "rails/health#show", as: :rails_health_check
+  get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
- end
+end
